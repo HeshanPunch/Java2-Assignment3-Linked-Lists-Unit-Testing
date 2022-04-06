7d59041bb11fe9f28ce0f1577fad2ee6ff10b3ff
Lauren Sung <lauren.sung23@gmail.com>
2022-04-06 17:19:42 -0600

    add node class

--- src/sait/sll/utility/Node.java
+++ src/sait/sll/utility/Node.java
@@ -1,5 +1,49 @@
 package sait.sll.utility;
 
 public class Node {
+	
+	private Object data;
+	private Node next;
+	
+	public Node(Object data, Node next) {
+		super();
+		this.data = data;
+		this.next = next;
+		
+		
+	}
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public void setSuccessor(Node node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Node getSuccessor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 
 }
