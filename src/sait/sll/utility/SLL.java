f4dec35e8bf8ffef2cee0f67be0ef908f6962665
Heshan <heshanpun@gmail.com>
2022-04-06 16:32:39 -0600

    Formatted with some javadoc comments

--- src/sait/sll/utility/SLL.java
+++ src/sait/sll/utility/SLL.java
@@ -2,70 +2,111 @@ package sait.sll.utility;
 
 
 public class SLL implements LinkedListADT {
-//testing Git
-	@Override
+
+	/**
+	 * Checks if the list is empty.
+	 * @return True if it is empty.
+	 */
 	public boolean isEmpty() {
-		// TODO Auto-generated method stub
 		return false;
 	}
+	
 
-	@Override
+	/**
+	 * Clears the list.
+	 */
 	public void clear() {
-		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
+	
+	/**
+	 * Adds to the end of the list.
+	 * @param data Data to append.
+	 */
 	public void append(Object data) {
-		// TODO Auto-generated method stub
-		
 	}
+	
 
-	@Override
+	/**
+	 * Prepends (adds to beginning) data to the list.
+	 * @param data Data to store inside element.
+	 */
 	public void prepend(Object data) {
-		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
+	
+	
+	/**
+	 * Adds a new element at a specific position.
+	 * @param data Data that element is to contain.
+	 * @param index Index to add new element at.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or past the size of the list.
+	 */
 	public void insert(Object data, int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		
 	}
+	
+	
 
-	@Override
+	/**
+	 * Replaces the data  at index.
+	 * @param data Data to replace.
+	 * @param index Index of element to replace.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger than size - 1 of list.
+	 */
 	public void replace(Object data, int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		
 	}
+	
+	
 
-	@Override
+	/**
+	 * Gets the number of elements in the list.
+	 * @return Size of list (0 meaning empty)
+	 */
 	public int size() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	
 
-	@Override
+	/**
+	 * Removes element at index from list, reducing the size.
+	 * @param index Index of element to remove.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or past the size - 1.
+	 */
 	public void delete(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
-		
 	}
-
-	@Override
+	
+	
+	
+	/**
+	 * Gets the data at the specified index.
+	 * @param index Index of element to get.
+	 * @return Data in element or null if it was not found.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger than size - 1 of the list.
+	 */
 	public Object retrieve(int index) throws IndexOutOfBoundsException {
-		// TODO Auto-generated method stub
 		return null;
 	}
+	
+	
 
-	@Override
+	/**
+	 * Gets the first index of element containing data.
+	 * @param data Data object to find the first index of.
+	 * @return First of index of element with matching data or -1 if not found.
+	 */
 	public int indexOf(Object data) {
-		// TODO Auto-generated method stub
 		return 0;
 	}
+	
+	
 
-	@Override
+	/**
+	 * Go through elements and check if we have one with data.
+	 * @param data Data object to search for.
+	 * @return True if element exists with value.
+	 */
 	public boolean contains(Object data) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
