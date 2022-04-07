b81c681a1766dfc6ef8093828f74940674dc59ca
Heshan <heshanpun@gmail.com>
2022-04-07 14:15:54 -0600

    append should work, working on the rest

--- src/sait/sll/utility/SLL.java
+++ src/sait/sll/utility/SLL.java
@@ -1,128 +1,144 @@
 package sait.sll.utility;
 
-
 public class SLL implements LinkedListADT {
-	
+
 	private Node head;
 	private Node tail;
-	
+
+	private int size = 0;
 
 	/**
 	 * Checks if the list is empty.
+	 * 
 	 * @return True if it is empty.
 	 */
 	public boolean isEmpty() {
+	if (head == tail && head == null) {
+		return true;
+	}else {
 		return false;
 	}
-	
+	}
 
 	/**
 	 * Clears the list.
 	 */
 	public void clear() {
 	}
-	
-	
+
 	/**
 	 * Adds to the end of the list.
+	 * 
 	 * @param data Data to append.
 	 */
 	public void append(Object data) {
-		if(!isEmpty()) {
+		System.out.println(data + " appending line31");
+		if (!isEmpty()) {
 			Node node = new Node(data);
 			tail.setNext(node);
 			tail = tail.getNext();
-		}else {
+		} else {
 			Node node = new Node(data);
 			tail = head = node;
 		}
+		size++;
 	}
-	
 
 	/**
 	 * Prepends (adds to beginning) data to the list.
+	 * 
 	 * @param data Data to store inside element.
 	 */
 	public void prepend(Object data) {
 		Node node = new Node(data, head);
 		head = node;
-		
-		if(tail == null) {
+
+		if (tail == null) {
 			tail = head;
 		}
-		
-		
+		size++;
+
 	}
-	
-	
-	
+
 	/**
 	 * Adds a new element at a specific position.
-	 * @param data Data that element is to contain.
+	 * 
+	 * @param data  Data that element is to contain.
 	 * @param index Index to add new element at.
-	 * @exception IndexOutOfBoundsException Thrown if index is negative or past the size of the list.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or past the
+	 *                                      size of the list.
 	 */
 	public void insert(Object data, int index) throws IndexOutOfBoundsException {
 	}
-	
-	
 
 	/**
-	 * Replaces the data  at index.
-	 * @param data Data to replace.
+	 * Replaces the data at index.
+	 * 
+	 * @param data  Data to replace.
 	 * @param index Index of element to replace.
-	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger than size - 1 of list.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger
+	 *                                      than size - 1 of list.
 	 */
 	public void replace(Object data, int index) throws IndexOutOfBoundsException {
 	}
-	
-	
 
 	/**
 	 * Gets the number of elements in the list.
+	 * 
 	 * @return Size of list (0 meaning empty)
 	 */
 	public int size() {
 		return 0;
 	}
-	
-	
 
 	/**
 	 * Removes element at index from list, reducing the size.
+	 * 
 	 * @param index Index of element to remove.
-	 * @exception IndexOutOfBoundsException Thrown if index is negative or past the size - 1.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or past the
+	 *                                      size - 1.
 	 */
 	public void delete(int index) throws IndexOutOfBoundsException {
 	}
-	
-	
-	
+
 	/**
 	 * Gets the data at the specified index.
+	 * 
 	 * @param index Index of element to get.
 	 * @return Data in element or null if it was not found.
-	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger than size - 1 of the list.
+	 * @exception IndexOutOfBoundsException Thrown if index is negative or larger
+	 *                                      than size - 1 of the list.
 	 */
 	public Object retrieve(int index) throws IndexOutOfBoundsException {
-		return null;
+		if (index < 0 || index >= size) {
+			return null;
+		} else if (index == 0) {
+			return head;
+
+		} else {
+			Node nodeAtIndex = head;
+			for (int i = 0; i < index; i++) {
+				nodeAtIndex = nodeAtIndex.getNext();
+			}
+			return nodeAtIndex;
+
+		}
+
 	}
-	
-	
 
 	/**
 	 * Gets the first index of element containing data.
+	 * 
 	 * @param data Data object to find the first index of.
 	 * @return First of index of element with matching data or -1 if not found.
 	 */
 	public int indexOf(Object data) {
 		return 0;
 	}
-	
-	
 
 	/**
 	 * Go through elements and check if we have one with data.
+	 * 
 	 * @param data Data object to search for.
 	 * @return True if element exists with value.
 	 */
--- src/sait/sll/utility/TestClass.java
+++
@@ -1,9 +0,0 @@
-package sait.sll.utility;
-
-public class TestClass {
-
-	public static void main(String[] args) {
-System.out.println("Hello Test");
-	}
-
-}
